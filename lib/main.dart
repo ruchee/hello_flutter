cdc0ecf678ec3d742dc87d1f29b3e162c0d1bfcd
Ruchee <my@ruchee.com>
2018-03-10 15:52:34 +0800

    step 3 done

--- lib/main.dart
+++ lib/main.dart
@@ -6,14 +6,25 @@ void main() => runApp(new MyApp());
 class MyApp extends StatelessWidget {
   @override
   Widget build(BuildContext context) {
-    final wordPair = new WordPair.random();
-
     return new MaterialApp(
       title: 'Hello Flutter',
       home: new Scaffold(
           appBar: new AppBar(title: new Text('你好 Flutter')),
-          body: new Center(child: new Text(wordPair.asPascalCase))
+          body: new Center(child: new RandomWords())
       ),
     );
+  }
+}
+
+class RandomWords extends StatefulWidget {
+  @override
+  createState() => new RandomWordsState();
+}
+
+class RandomWordsState extends State<RandomWords> {
+  @override
+  Widget build(BuildContext context) {
+    final wordPair = new WordPair.random();
+    return new Text(wordPair.asPascalCase);
   }
 }
